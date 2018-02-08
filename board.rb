d58a936e2acbf376fe613a1d6d7c3a43714907a0
Wendy Huang <wendy.huang@wework.com>
2018-02-08 06:13:15 +0000

    automatically backed up by learn

--- board.rb
+++ board.rb
@@ -2,11 +2,11 @@ board = ["x", " ", " ", " ", " ", " ", " ", " ", " "]
 
 
 def display_board
-  puts "  #{board[0]} | #{board[1]}  |   "
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "---------"
-  puts "   |   |   "
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "---------"
-  puts "   |   |   "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 display_board
