21600284d3add0a1204df79a97a98bc1d5925150
Wendy Huang <wendy.huang@wework.com>
2018-02-08 06:19:18 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,10 +1,9 @@
+board = ["x", " ", " ", "o", " ", " ", " ", " ", " "]
 
-def display_board
-  puts "   |   |   "
-  puts "---------"
-  puts "   |   |   "
-  puts "---------"
-  puts "   |   |   "
-end
 
-display_board
+puts " #{board[0]} | #{board[1]} | #{board[2]} "
+puts "---------"
+puts " #{board[3]} | #{board[4]} | #{board[5]} "
+puts "---------"
+puts " #{board[6]} | #{board[7]} | #{board[8]} "
+
